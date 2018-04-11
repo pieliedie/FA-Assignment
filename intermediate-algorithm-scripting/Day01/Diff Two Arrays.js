1b870302afda6d76feb2f844e4a6fda133d6c735
Do Tien Hai <dotienhai1611@gmail.com>
2018-04-11 21:21:02 +0700

    set up node modules

--- intermediate-algorithm-scripting/Day01/Diff Two Arrays.js
+++ intermediate-algorithm-scripting/Day01/Diff Two Arrays.js
@@ -1,17 +1,17 @@
-let diffArray = (arr1, arr2) => {
+const diffArray = (arr1, arr2) => {
   const newArr = [];
-  let onlyInFirst = (firstArr, secondArr) => {
-    for (let i=0; i<firstArr.length; i++) {
+  const onlyInFirst = (firstArr, secondArr) => {
+    for (let i = 0; i < firstArr.length; i++) {
       if (!secondArr.includes(firstArr[i])) {
         newArr.push(firstArr[i]);
       }
     }
-  }
-  
+  };
+
   onlyInFirst(arr1, arr2);
   onlyInFirst(arr2, arr1);
-  
+
   return newArr;
-}
+};
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
